bca4ad3a0b45cf5ccd7d33760f31ed0685d56208
Pankaj Puri <pankajpuri016@gmail.com>
2022-11-09 18:46:13 +1300

    checking the update

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import getJobDatas from "./datas/jobDatas";
 class Work extends Component {
-  constructor(props) {
-    super(props);
-  }
   state = { datas: getJobDatas() };
+
   render() {
     return (
       <section id="slider" className="container-fluid mb-5 mt-5 pt-5">
@@ -14,18 +12,13 @@ class Work extends Component {
           </h1>
           <div className="slider">
             <div className="owl-carousel">
-              {/* <div class="slider-card">
-                <div class="d-flex justify-content-center align-items-center mb-4">
-                  <img src="../Images/introductions/1.jpeg" alt="" />
-                </div>
-              </div> */}
               {this.state.datas.map((data) => (
                 <div className="slider-card mb-3" key={data.id}>
                   <div
                     key={data.id}
                     className="d-flex justify-content-center align-items-center mb-4"
                   >
-                    <img src={data.intSource} alt=""></img>
+                    <img src={data.intSource} alt="" />
                   </div>
                 </div>
               ))}
