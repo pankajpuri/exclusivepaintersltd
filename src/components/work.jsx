effea10223776d26d328948b08bb8267fe2e2089
Pankaj Puri <pankajpuri016@gmail.com>
2022-11-09 18:04:18 +1300

    checking the update

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,53 +1,40 @@
-import React from "react";
-const Work = () => {
-  return (
-    <section id="slider" className="container-fluid mb-5 mt-5 pt-5">
-      <div className="row">
-        <h1 class="text-center">
-          <b>Our Projects</b>
-        </h1>
-        <div class="slider">
-          <div class="owl-carousel">
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/1.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/2.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/3.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/4.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/11.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/55.jpeg" alt="" />
-              </div>
-            </div>
-            <div class="slider-card">
-              <div class="d-flex justify-content-center align-items-center mb-4">
-                <img src="../Images/introductions/88.jpeg" alt="" />
-              </div>
+import React, { Component } from "react";
+import getJobDatas from "./datas/jobDatas";
+class Work extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = { datas: getJobDatas() };
+  render() {
+    return (
+      <section id="slider" className="container-fluid mb-5 mt-5 pt-5">
+        <div className="row">
+          <h1 className="text-center">
+            <b>Our Projects</b>
+          </h1>
+          <div className="slider">
+            <div className="owl-carousel">
+              {/* <div class="slider-card">
+                <div class="d-flex justify-content-center align-items-center mb-4">
+                  <img src="../Images/introductions/1.jpeg" alt="" />
+                </div>
+              </div> */}
+              {this.state.datas.map((data) => (
+                <div className="slider-card mb-3" key={data.id}>
+                  <div
+                    key={data.id}
+                    className="d-flex justify-content-center align-items-center mb-4"
+                  >
+                    <img src={data.intSource} alt=""></img>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default Work;
