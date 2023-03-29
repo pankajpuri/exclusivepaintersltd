7764415a2901e1797904a3d85c89203d16a357a4
Pankaj Puri <pankajpuri016@gmail.com>
2023-03-30 11:04:01 +1300

    changed spell

--- src/components/contract.jsx
+++ src/components/contract.jsx
@@ -4,7 +4,7 @@ const Contract = () => {
     <section className="product" id="services">
       <div className="container">
         <div className="row product-marginbtm50">
-          <h3 className="product_title">Crontract</h3>
+          <h3 className="product_title">Contract</h3>
           <p>Coming soon!</p>
         </div>
       </div>
